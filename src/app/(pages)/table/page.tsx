2477b0f741800e94160a043e5978e9cf18f26ca6
miyahara <50367145+subretu@users.noreply.github.com>
2025-01-12 23:42:07 +0900

    Add metadata

--- src/app/(pages)/table/page.tsx
+++ src/app/(pages)/table/page.tsx
@@ -1,4 +1,11 @@
 import TableClient from "@/components/tableClient";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Home",
+  };
+}
 
 export default function DenseTableServer() {
   return (
