d0aa5f258ad2c694c7e04e11239546fead2f6e19
miyahara <50367145+subretu@users.noreply.github.com>
2025-01-22 23:39:49 +0900

    try prosma migration

---
+++ prisma_migration/prisma/migrations/20250122141322_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "customer_user" (
+    "id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT,
+    "name" TEXT,
+
+    CONSTRAINT "customer_user_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma_migration/prisma/migrations/20250122141837_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "customer" (
+    "id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "customer_pkey" PRIMARY KEY ("id")
+);
